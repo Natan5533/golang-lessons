faafd4a56852792e3824d4d840d4285d9ab1b6a8
Natan5533 <naelramosfilho@gmail.com>
2023-03-14 14:52:54 -0300

    first lesson

---
+++ ola/ola.go
@@ -0,0 +1,31 @@
+package main
+
+const prefixInEnglish = "Hello "
+const prefixInPortuguese = "Eae, "
+const prefixInFrench = "Bonjour "
+const portuguese = "portuguese"
+const french = "french"
+
+func Ola(name string, language string) string {
+    if name == "" {
+        name = "World"
+    }
+    return prefixResponse(language) + name 
+}
+
+func prefixResponse(language string)(prefix string){
+    switch language {
+    case french:
+        prefix = prefixInFrench
+    case portuguese:
+        prefix = prefixInPortuguese
+    default:
+        prefix = prefixInEnglish
+    }
+	return  
+}
+
+
+
+func main() {
+}
