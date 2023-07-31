2eb151dc2d9fdcc9a08fce73deb7a6866e9a6dc1
Natan5533 <naelramosfilho@gmail.com>
2023-07-30 21:01:58 -0300

    Lists(?arrays and slices?)

---
+++ arrays/array_test.go
@@ -0,0 +1,21 @@
+package arrays
+
+import "testing"
+
+func TestListLength(t *testing.T) {
+	assert := func(t *testing.T, result, expected int, array []int) {
+		t.Helper()
+		if expected != result {
+			t.Errorf("Result: %d - Expected: %d List: %v", result, expected, array)
+		}
+	}
+	t.Run("slice", func(t *testing.T) {
+		slice := []int{5, 5}
+
+		result := ListCounter(slice)
+		expected := 10
+
+		assert(t, result, expected, slice)
+
+	})
+}
