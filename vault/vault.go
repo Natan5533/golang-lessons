79ed15215be9551c843588b01310dbe97d48f326
Natan5533 <naelramosfilho@gmail.com>
2023-08-20 13:43:14 -0300

    maps in golang

--- vault/vault.go
+++ vault/vault.go
@@ -1,11 +1,72 @@
 package vault
 
-func Create(key string, password string) map[string]string {
-	newVault := map[string]string{key: password}
+type Vault map[string]string
 
-	return newVault
+type ErrorsVault string
+
+const (
+	ErrorNotExist     = ErrorsVault("password does not exist")
+	ErrorAlreadyExist = ErrorsVault("this password has already been created")
+)
+
+func (e ErrorsVault) Error() string {
+	return string(e)
 }
 
-func Fetch(vault map[string]string, key string) string {
-	return vault[key]
+func (v Vault) Create(key, password string) error {
+
+	_, err := v.Fetch(key)
+
+	switch err {
+	case ErrorNotExist:
+		v[key] = password
+	case nil:
+		return ErrorAlreadyExist
+	default:
+		return err
+	}
+
+	return nil
 }
+
+func (v Vault) Fetch(key string) (string, error) {
+	result, exist := v[key]
+
+	if !exist {
+		return "", ErrorNotExist
+	}
+
+	return result, nil
+}
+
+func (v Vault) Update(key, newPass string) error {
+	_, err := v.Fetch(key)
+
+	switch err {
+	case ErrorNotExist:
+		return ErrorNotExist
+	case nil:
+		v[key] = newPass
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (v Vault) Delete(key string) error {
+	_, err := v.Fetch(key)
+
+	switch err {
+	case ErrorNotExist:
+		return ErrorNotExist
+	case nil:
+		delete(v, key)
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// func CheckKey(key s)
--- vault/vault_test.go
+++ vault/vault_test.go
@@ -1,13 +1,100 @@
 package vault
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFetch(t *testing.T) {
-	vault := Create("aws", "asmnk-fjgnsd-sjfdb")
-	result := Fetch(vault, "aws")
-	expected := "asmnk-fjgnsd-sjfdb"
+	//vault := Create("aws", "asmnk-fjgnsd-sjfdb")
+	vault := Vault{"aws": "asmnk-fjgnsd-sjfdb"}
+
+	t.Run("Exists? == True", func(t *testing.T) {
+		result, err := vault.Fetch("aws")
+		expected := "asmnk-fjgnsd-sjfdb"
+
+		assertError(t, err, nil)
+		assertEqual(t, result, expected)
+	})
+
+	t.Run("Exists? == False", func(t *testing.T) {
+		output, err := vault.Fetch("netflix passoword")
+		expectedOutput := ""
+
+		assertEqual(t, output, expectedOutput)
+		assertError(t, err, ErrorNotExist)
+	})
+
+}
+
+func TestCreatePass(t *testing.T) {
+
+	t.Run("Create password", func(t *testing.T) {
+		vault := Vault{}
+		vault.Create("netflix", "12345678_this_is_a_bad_password")
+
+		expected := "12345678_this_is_a_bad_password"
+
+		result, err := vault.Fetch("netflix")
+
+		assertError(t, err, nil)
+		assertEqual(t, result, expected)
+	})
+
+	t.Run("Passord already exists", func(t *testing.T) {
+		vault := Vault{}
+
+		errNil := vault.Create("pass", "some_password")
+		err := vault.Create("pass", "x-x-x-x-x-x-x")
+		password, _ := vault.Fetch("pass")
+		expected := "some_password"
+
+		assertError(t, errNil, nil)
+		assertError(t, err, ErrorAlreadyExist)
+		assertEqual(t, password, expected)
+	})
+
+}
+func TestUpdatePassword(t *testing.T) {
+	vault := Vault{}
+
+	vault.Create("device", "some_password")
+	err := vault.Update("device", "new_password")
+
+	password, _ := vault.Fetch("device")
+
+	expected := "new_password"
+
+	assertEqual(t, password, expected)
+	assertError(t, err, nil)
+}
+
+// I'm too lazy to do the failure tests but I know it's functional haha
+
+func TestDeletePassword(t *testing.T) {
+	vault := Vault{}
+
+	vault.Create("device", "some_password")
+	vault.Delete("device")
+
+	_, err := vault.Fetch("device")
+
+	assertError(t, err, ErrorNotExist)
+}
+
+// Test functions
+
+func assertError(t *testing.T, result, expected error) {
+	t.Helper()
 
 	if result != expected {
-		t.Errorf("Result: %s Expected: %s", result, expected)
+		t.Errorf("[failed] Result: %s Expected: %s", result, expected)
+	}
+}
+
+func assertEqual(t *testing.T, result, expected string) {
+	t.Helper()
+
+	if result != expected {
+		t.Errorf("[failed] Result: %s Expected: %s", result, expected)
 	}
 }
