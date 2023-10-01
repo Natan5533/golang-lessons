6e015f561da700fe369c963cad48127108b281f2
Natana <naelramosfilho@gmail.com>
2023-10-01 19:23:06 -0300

    Understanding go routines and concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,13 +1,29 @@
 package concurrency
 
-type WebSiteChecker func(string) bool
+import "time"
 
-func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+type WebSiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebSiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	time.Sleep(2 * time.Second)
 
 	return results
 
